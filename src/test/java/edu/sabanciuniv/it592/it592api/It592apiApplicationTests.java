158799b57d56e4610ad85d70e4f3ce0c8d64ef23
mkilic <mkilic.ktu@gmail.com>
2021-08-02 01:12:55 +0300

    first issue

---
+++ src/test/java/edu/sabanciuniv/it592/it592api/It592apiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.it592.it592api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It592apiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
