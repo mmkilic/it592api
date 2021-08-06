4f752870efd9a9c874e5bda2d8ce0ec0301a5ef7
mkilic <mkilic.ktu@gmail.com>
2021-08-06 12:26:29 +0300

    first classes

---
+++ src/main/java/edu/sabanciuniv/it592api/entity/Project.java
@@ -0,0 +1,11 @@
+package edu.sabanciuniv.it592api.entity;
+
+public class Project {
+	
+	private long id;
+	private int projectNumber;
+	private String elecDesigner;
+	private String mechDesigner;
+	
+
+}
